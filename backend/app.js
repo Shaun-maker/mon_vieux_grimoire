0fe5ee94962e28217f29fb3db21683b88af98630
Axel Paillaud <a.paillaud75@gmail.com>
2023-05-11 06:02:41 +0200

    create routes api/books/ GET

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,14 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.status(201);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-})
+  });
 
-app.use((req, res, next) => {
-    res.json({message: "Votre requête a bien été reçue !"});
-});
+const bookRoutes = require('./routes/book');
+
+app.use('/api/books', bookRoutes);
 
 module.exports = app;
