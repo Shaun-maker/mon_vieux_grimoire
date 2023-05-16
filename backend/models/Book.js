77f2521740d084d7f947cbaeff15b1465483f330
Axel Paillaud <a.paillaud75@gmail.com>
2023-05-16 06:14:30 +0200

    update books schema, follow openClassrooms specifications

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -5,9 +5,14 @@ const bookSchema = mongoose.Schema({
     title: {type: String, required: true, unique: true},
     author: {type: String, required: true},
     year: {type: Number, required: true},
-    urlImage: {type: String, required: true},
-    rates: {type: Number},
-    type: {type: String, required: true}
+    imageUrl: {type: String, required: true},
+    genre: {type: String, required: true},
+    ratings: [
+        {
+            userId: {type: String, required: true},
+            grade: {type: Number, required: true}
+        }
+    ]
 });
 
 module.exports = mongoose.model('Book', bookSchema);
