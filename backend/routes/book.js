b551cee90b7655bbd2a26128f1692edd4a4a3192
Axel Paillaud <a.paillaud75@gmail.com>
2023-05-16 06:12:36 +0200

    add auth middleware on routes that need authentification

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -7,8 +7,8 @@ const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBooks);
 router.get('/:id', bookCtrl.getOneBooks);
-router.post('/', bookCtrl.createBook);
-router.put('/:id', bookCtrl.modifyBook);
-router.delete('/:id', bookCtrl.deleteBook);
+router.post('/', auth, bookCtrl.createBook);
+router.put('/:id', auth, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
